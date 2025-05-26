a150d36046a5d7fb5bbaec65deaba99cc39f0ecc
Angel MR <angeldanielmero@hotmail.com>
2025-05-25 21:33:21 -0600

    fix: corregir integración de rutas y props en Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Navbar from './components/Navbar'
-import Events from './components/Events'
-import SignUpForm from './components/SignUpForm'
 import  Routes  from './routes'
 
 function App() {
-  /*const [searchText, setSearchText] = useState('');
-
-  function handleNavbarSearch(text){
-      setSearchText(text);
-  };
-
-
-  return (
-    <>
-      <Navbar onSearch={handleNavbarSearch} />
-      <Events searchText={searchText}/>
-    </>
-  )*/
+  
 return <Routes/>
 }
 
--- src/view/HOme/index.jsx
+++ src/view/HOme/index.jsx
@@ -1,3 +1,21 @@
-const Home = () => <div>Home</div>
+import Navbar from '../../components/Navbar'
+import Events from '../../components/Events'
+import { useState } from 'react';
+
+const Home = () => {
+    const [searchText, setSearchText] = useState('');
+
+    function handleNavbarSearch(text) {
+        setSearchText(text);
+    };
+
+
+    return (
+        <>
+            <Navbar onSearch={handleNavbarSearch} />
+            <Events searchText={searchText} />
+        </>
+    )
+}
 
 export default Home;
