17d08b86bd2739d90eac84a05a1b64ad1307e573
Angel MR <angeldanielmero@hotmail.com>
2025-05-25 21:27:11 -0600

    feat:  estilos CSS para vistas y componentes

--- src/components/Events/Components/EventItem/index.jsx
+++ src/components/Events/Components/EventItem/index.jsx
@@ -1,16 +1,28 @@
+import styles from './EventItem.module.css';
+
+
+
+
 const EventItem = ({id,info,name,image, onEventClick}) => {
 
-    function handelSeeMoreClick (event) {
-        event.stopPropagation();
-        onEventClick(id)
-    }
+    
+    const handleSeeMoreClick = (evt) => {
+        evt.stopPropagation();
+        onEventClick(id);
+    };
+
+
 
     return (
-        <div >
-            <img src= {image} alt = {name}/>
-            <h4>{name}</h4>
-            <p>{info}</p>
-            <button onClick={handelSeeMoreClick}>Ver mas</button>
+         <div onClick={() => console.log('padre clickeado')} className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
+            <img src={image} alt={name} width={200} height={200} />
+            <div className={styles.eventInfoContainer}>
+                <h4 className={styles.eventName}>{name}</h4>
+                <p className={styles.eventInfo}>{info}</p>
+                <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>
+                    Ver mas
+                </button>
+            </div>
         </div>
     );
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 //pasar propiedad desde app
-const Navbar = ({onSearch}) => {
+const Navbar = ({ onSearch }, ref) => {
 
   //Valor de useState con comillas
   const [search, setSearch] = useState('');
@@ -10,22 +10,40 @@ const Navbar = ({onSearch}) => {
     setSearch(event.target.value);
   }
   function handleInputKeyDown(event) {
-    if(event.key === 'Enter'){
+    if (event.key === 'Enter') {
       //actualiza estado para la busqueda con lo que recibe desde el state
       onSearch(search);
     }
   }
 
   return (
-    <div>
-      <p>Mi boletera</p>
-      <input
-        placeholder="Busca tu evento favorito"
-        onChange={handleInputChange}
-        onKeyDown={handleInputKeyDown}
-        value={search}
+    <div ref={ref} style={{
+      marginBottom: 14,
+      width: '100%',
+      display: 'flex',
+    }}>
+      <div style={{ flex: 1, display: 'flex' }}>
+        <p style={{
+          fontSize: 24,
+          fontWeight: 'bold'
+        }}>Mi boletera</p>
+      </div>
+      <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+        <input
+          placeholder="Busca tu evento favorito"
+          onChange={handleInputChange}
+          onKeyDown={handleInputKeyDown}
+          value={search}
+          style={{
+            fontSize: 16,
+            padding: '6px 12px',
+            borderRadius: 4,
+            border: 'none',
+            width: 200,
+          }}
+        />
 
-      />
+      </div>
     </div>
   );
 };
